df9f12ea881cf89883ba57d45a31dcfb3c567d76
Charanya Chowdary M <charanyachowdarym@gmail.com>
2026-06-25 16:11:32 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq={}
+        if len(s)!=len(t):
+            return False
+        for i in s:
+            freq[i]=freq.get(i,0)+1
+        for i in t:
+            if i not in freq:
+                return False
+            freq[i]-=1
+            if freq[i]<0:
+                return False
+        return True
