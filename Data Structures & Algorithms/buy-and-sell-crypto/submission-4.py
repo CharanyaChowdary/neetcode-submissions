160572c70098696eab81f63e58eae0107316015f
Charanya Chowdary M <charanyachowdarym@gmail.com>
2026-07-03 22:00:54 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit=0
+        min_buy=prices[0]
+        for i in prices:
+            min_buy=min(min_buy,i)
+            max_profit=max(max_profit,i-min_buy)
+        return max_profit
+        
+
+        
